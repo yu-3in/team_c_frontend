cf786a81a9989d34ae27347a5d51082c0d49182b
yuuumiravy <hikoki.y@gmail.com>
2023-08-02 07:26:06 +0900

    feat: チケット編集SidePanel

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages'
 import Calendar from './pages/Calendar'
 import { Tickets } from './pages/tickets'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import ja from 'dayjs/locale/ja'
 
 const App = () => {
+  console.log(ja.name)
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="tickets" element={<Tickets />} />
-      </Routes>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={ja.name}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="tickets" element={<Tickets />} />
+        </Routes>
+      </LocalizationProvider>
     </BrowserRouter>
   )
 }

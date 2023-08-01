3c82a2804903c477fda6dbab35e71ec2a69cfab2
yuuumiravy <hikoki.y@gmail.com>
2023-08-01 11:48:44 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@material-tailwind/react";
+
+const App = () => (
+    <div className="container mx-auto bg-red-400">
+      <Button>ホゲ</Button>
+    </div>
+  )
+
+export default App;

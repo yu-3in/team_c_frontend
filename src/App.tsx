35671f822cd3425307c40f3c3d43b10d656999b1
shooh25 <koan6210@outlook.jp>
2023-08-01 15:45:16 +0900

    add calender route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages'
+import Calender from './pages/Calender'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/calender" element={<Calender />} />
       </Routes>
     </BrowserRouter>
   )

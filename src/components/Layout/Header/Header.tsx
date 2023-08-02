9d72b4c65740b61c8a28e9b5b3bd7d902b68be04
Yota KUMANO <130555114+Kumano-Yota@users.noreply.github.com>
2023-08-03 01:46:38 +0900

    Update Header.tsx

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -18,7 +18,7 @@ export const Header: React.FC<HeaderProps> = () => {
   return (
     <header>
       <Navbar className="mx-auto my-2 max-w-[95%] px-4 py-2">
-        <div className="flex items-center justify-between text-blue-gray-900">
+         <div className="flex items-center justify-between text-blue-gray-900">
           <div className="items-start">
             <h6
               className="cursor-pointer py-1.5 text-inherit lg:ml-2 ml-0"
@@ -27,7 +27,7 @@ export const Header: React.FC<HeaderProps> = () => {
               Header
             </h6>
           </div>
-          <div className="flex">
+          <div className="flex items-center">
           <NavList />
             <div className="hidden gap-2 lg:flex">
               {/* <Button variant="text" color="blue-gray">

09c84388680b8f05a3773641297adef5cf0431b5
shooh25 <koan6210@outlook.jp>
2023-08-03 01:27:36 +0900

    update

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -7,8 +7,11 @@ import jaLocale from '@fullcalendar/core/locales/ja'
 import styles from '../styles/calendar.module.css'
 import { Layout } from '../components/Layout/Layout'
 import { Link } from 'react-router-dom'
+import { useQuery } from 'react-query'
+// import { Ticket } from '../types/ticket'
+import { User } from '../types/user'
+import { getUsers } from '../apis/user'
 
-// カレンダーに関するスタイル
 import '../styles/calendar-global.css'
 
 type eventType = {
@@ -22,34 +25,34 @@ const Calendar = () => {
   const userSample = [
     { name: '名前 なまえ', pos: 'GMOインターネット', id: 1 },
     { name: '名前 なまえ', pos: 'GMOインターネット', id: 2 },
-    { name: '名前 なまえ', pos: 'GMOインターネット', id: 3 },
-    { name: '名前 なまえ', pos: 'GMOインターネット', id: 4 },
   ]
-
   // チケットのサンプルデータ
   const eventSample = [
     { title: '要件定義書を作成', start: '2023-08-01T10:00:00', end: '2023-08-01T14:00:00' },
-    { title: 'hoge', start: '2023-08-02T10:00:00', end: '2023-08-02T16:00:00' },
-    { title: 'hoge', start: '2023-08-03T10:00:00', end: '2023-08-03T16:00:00' },
-    { title: 'hoge', start: '2023-08-04T10:00:00', end: '2023-08-04T16:00:00' },
   ]
+
+  // const [tickets, setTickets] = useState<Ticket[]>([])
+  const [users, setUsers] = useState<User[]>([])
+
+  const getUsersQuery = useQuery(['users'], () => getUsers(), {
+    onSuccess: (res) => setUsers(res),
+  })
+
+  console.log(users)
 
   // チケットをクリックしたとき
   const handleClick = (event: eventType) => {
     alert(event.title)
   }
 
-  // チケット作成
-  const createTicket = () => {
-    alert()
-  }
+  if (getUsersQuery.isLoading) return <></>
 
   return (
     <Layout>
       <div className={styles.contents}>
         <div className={styles.wrapper}>
           <div className={styles.side}>
-            <button onClick={createTicket}>チケット作成</button>
+            <button onClick={() => console.log()}>チケット作成</button>
             <div className={styles.userHeading}>
               <h1>ユーザーリスト</h1>
             </div>

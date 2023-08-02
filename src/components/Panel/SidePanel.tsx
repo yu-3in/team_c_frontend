9508ee05521e00cb6d847d96efc5a5f7bc1b3c13
shooh25 <koan6210@outlook.jp>
2023-08-02 21:47:54 +0900

    del button

--- src/components/Panel/SidePanel.tsx
+++ src/components/Panel/SidePanel.tsx
@@ -1,6 +1,5 @@
 import { Drawer, IconButton } from '@mui/material'
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft'
-import { Button } from "../Button/Button"
 
 type DrawerProps = {
   open: boolean
@@ -18,34 +17,30 @@ export const SidePanel: React.FC<DrawerProps> = ({
   children,
 }) => {
   return (
-    <>
-      <Drawer
-        open={open}
-        onClose={onClose}
-        anchor={placement}
-        PaperProps={{
-          sx: { width: '50vw' },
-        }}>
-        <div className="mx-auto w-full px-4 py-6">
-          <div className="mx-4 mb-2 flex items-center gap-4 border-gray-500 text-gray-500">
-            <IconButton
-              sx={{
-                width: 16,
-                height: 16,
-                border: '1px solid #757575',
-                padding: 2.3,
-                borderRadius: 2,
-              }}
-              onClick={onClose}>
-              <KeyboardArrowLeftIcon />
-            </IconButton>
-            <h3 className="my-4 text-2xl font-bold text-gray-800">{title}</h3>
-          </div>
-          {children}
+    <Drawer
+      open={open}
+      onClose={onClose}
+      anchor={placement}
+      PaperProps={{
+        sx: { width: '50vw' },
+      }}>
+      <div className="mx-auto w-full px-4 py-6">
+        <div className="mx-4 mb-2 flex items-center gap-4 border-gray-500 text-gray-500">
+          <IconButton
+            sx={{
+              width: 16,
+              height: 16,
+              border: '1px solid #757575',
+              padding: 2.3,
+              borderRadius: 2,
+            }}
+            onClick={onClose}>
+            <KeyboardArrowLeftIcon />
+          </IconButton>
+          <h3 className="my-4 text-2xl font-bold text-gray-800">{title}</h3>
         </div>
-      </Drawer>
-      <Button variant="outline">キャンセル</Button>
-      <Button variant="fill">作成する</Button>
-    </>
+        {children}
+      </div>
+    </Drawer>
   )
 }

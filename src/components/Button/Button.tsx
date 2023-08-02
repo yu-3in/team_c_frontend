fc09e99844b33bc5221d5aadd239cdeb6668d6f4
Yota KUMANO <130555114+Kumano-Yota@users.noreply.github.com>
2023-08-02 11:42:37 +0900

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import styles from './Button.module.css'
 
 type ButtonProps = {
-  // children:React.ReactNode
-  value: string
+  children: React.ReactNode
+  variant?: 'fill' | 'outline'
 }
-export const Button: React.FC<ButtonProps> = ({ value }) => {
-  return <button className={styles.button}>{value}</button>
+export const Button: React.FC<ButtonProps> = ({ children, variant = 'fill' }) => {
+  return (
+    <button className={variant === 'fill' ? styles.button_fill : styles.button_outline}>
+      {children}
+    </button>
+  )
 }

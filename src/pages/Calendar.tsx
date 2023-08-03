125c057b494f7b378d98f0f8c3b3819075b1556b
shooh25 <koan6210@outlook.jp>
2023-08-03 10:49:15 +0900

    add user list

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,101 +1,110 @@
-import { useState } from 'react'
-import FullCalendar from '@fullcalendar/react'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import jaLocale from '@fullcalendar/core/locales/ja'
-import styles from '../styles/calendar.module.css'
-import { Layout } from '../components/Layout/Layout'
-import { Link } from 'react-router-dom'
-import { useQuery } from 'react-query'
+// import { useState } from 'react'
+// import FullCalendar from '@fullcalendar/react'
+// import timeGridPlugin from '@fullcalendar/timegrid'
+// import dayGridPlugin from '@fullcalendar/daygrid'
+// import interactionPlugin from '@fullcalendar/interaction'
+// import jaLocale from '@fullcalendar/core/locales/ja'
+// import styles from '../styles/calendar.module.css'
+// import { Layout } from '../components/Layout/Layout'
+// import { Link } from 'react-router-dom'
+// import { useQuery } from 'react-query'
 // import { Ticket } from '../types/ticket'
-import { User } from '../types/user'
-import { getUsers } from '../apis/user'
+// import { User } from '../types/user'
+// import { getUsers } from '../apis/user'
+// import { getTickets } from '../apis/ticket'
+// import '../styles/calendar-global.css'
 
-import '../styles/calendar-global.css'
+// type EventType = {
+//   title: string
+//   start: Date | null
+//   end: Date | null
+// }
 
-type eventType = {
-  title: string
-  start: Date | null
-  end: Date | null
-}
+// const Calendar = () => {
+//   const eventSample = [
+//     { title: '要件定義書を作成', start: '2023-08-01T10:00:00', end: '2023-08-01T14:00:00', id:"10"},
+//     // {
+//     //   id: 10,
+//     //   title: 'string',
+//     //   description: 'string',
+//     //   status: 'todo',
+//     //   dueDate: '2023-08-01T10:00:00',
+//     //   start: '2023-08-01T10:00:00',
+//     //   end: '2023-08-01T14:00:00',
+//     //   User: {},
+//     //   Genre: [],
+//     //   createdAt: 'string',
+//     //   updatedAt: 'string',
+//     // },
+//   ]
 
-const Calendar = () => {
-  // ユーザーのサンプルデータ
-  const userSample = [
-    { name: '名前 なまえ', pos: 'GMOインターネット', id: 1 },
-    { name: '名前 なまえ', pos: 'GMOインターネット', id: 2 },
-  ]
-  // チケットのサンプルデータ
-  const eventSample = [
-    { title: '要件定義書を作成', start: '2023-08-01T10:00:00', end: '2023-08-01T14:00:00' },
-  ]
+//   const [tickets, setTickets] = useState<[]>([])
+//   const [users, setUsers] = useState<User[]>([])
 
-  // const [tickets, setTickets] = useState<Ticket[]>([])
-  const [users, setUsers] = useState<User[]>([])
+//   const getUsersQuery = useQuery(['users'], () => getUsers(), {
+//     onSuccess: (res) => setUsers(res),
+//   })
 
-  const getUsersQuery = useQuery(['users'], () => getUsers(), {
-    onSuccess: (res) => setUsers(res),
-  })
+//   const getTicketsQuery = useQuery(['tickets'], () => getTickets(), {
+//     // onSuccess: (res) => setTickets(),
+//   })
 
-  console.log(users)
+//   const handleClick = (event: EventType) => {
+//     alert(event.title)
+//   }
 
-  // チケットをクリックしたとき
-  const handleClick = (event: eventType) => {
-    alert(event.title)
-  }
+//   if (getUsersQuery.isLoading) return <>ロード中</>
+//   // if (getTicketsQuery.isLoading) return <>チケットデータをロード中</>
 
-  if (getUsersQuery.isLoading) return <></>
+//   return (
+//     <Layout>
+//       <div className={styles.contents}>
+//         <div className={styles.wrapper}>
+//           <div className={styles.side}>
+//             <button onClick={() => console.log()}>チケット作成</button>
+//             <div className={styles.userHeading}>
+//               <h1>ユーザーリスト</h1>
+//             </div>
+//             <div className={styles.userList}>
+//               {users.map((user) => (
+//                 <Link to="/user" key={user.id}>
+//                   <div className={styles.userData}>
+//                     <div className={styles.iconArea}></div>
+//                     <div className={styles.content}>
+//                       <p className={styles.name}>{user.name}</p>
+//                       <p className={styles.pos}>{user.email}</p>
+//                     </div>
+//                   </div>
+//                 </Link>
+//               ))}
+//             </div>
+//           </div>
+//           <div className={styles.calendar}>
+//             <FullCalendar
+//               height="100%"
+//               plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
+//               initialView="timeGridWeek"
+//               locales={[jaLocale]}
+//               locale="ja"
+//               headerToolbar={{
+//                 left: 'dayGridMonth,timeGridWeek,timeGridDay',
+//                 center: '',
+//                 right: 'prev,next',
+//               }}
+//               events={eventSample}
+//               eventClick={(arg) =>
+//                 handleClick({
+//                   title: arg.event.title,
+//                   start: arg.event.start,
+//                   end: arg.event.end,
+//                 })
+//               }
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </Layout>
+//   )
+// }
 
-  return (
-    <Layout>
-      <div className={styles.contents}>
-        <div className={styles.wrapper}>
-          <div className={styles.side}>
-            <button onClick={() => console.log()}>チケット作成</button>
-            <div className={styles.userHeading}>
-              <h1>ユーザーリスト</h1>
-            </div>
-            <div className={styles.userList}>
-              {userSample.map((user) => (
-                <Link to="/user" key={user.id}>
-                  <div className={styles.userData}>
-                    <div className={styles.iconArea}></div>
-                    <div className={styles.content}>
-                      <p className={styles.name}>{user.name}</p>
-                      <p className={styles.pos}>{user.pos}</p>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className={styles.calendar}>
-            <FullCalendar
-              height="100%"
-              plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
-              initialView="timeGridWeek"
-              locales={[jaLocale]}
-              locale="ja"
-              headerToolbar={{
-                left: 'dayGridMonth,timeGridWeek,timeGridDay',
-                center: '',
-                right: 'prev,next',
-              }}
-              events={eventSample}
-              eventClick={(arg) =>
-                handleClick({
-                  title: arg.event.title,
-                  start: arg.event.start,
-                  end: arg.event.end,
-                })
-              }
-            />
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
-}
-
-export default Calendar
+// export default Calendar

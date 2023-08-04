f3558bfa265e76f392e9abfea522667d0c6de30a
shooh25 <koan6210@outlook.jp>
2023-08-04 12:07:48 +0900

    fix home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import dayGridPlugin from '@fullcalendar/daygrid'
 import { Layout } from '../components/Layout/Layout'
 import { SidePanel } from '../components/Panel/SidePanel'
 import { useCallback, useState } from 'react'
@@ -14,22 +15,53 @@ import { deleteTicket, getTickets } from '../apis/ticket'
 import { Status } from '../types/status'
 import { getMe } from '../apis/user'
 
+type EventType = {
+  id: string
+  title: string
+  start: string | undefined
+  end: string | undefined
+  userId?: number
+}
+
 export const Home: React.FC = () => {
+  const [events, setEvents] = useState<EventType[]>([])
   const [openCreateDrawer, setOpenCreateDrawer] = useState(false)
   const [openEditDrawer, setOpenEditDrawer] = useState(false)
   const [clickedTicket, setClickedTicket] = useState<Ticket>()
   const [targetStatus, setTargetStatus] = useState<Status>()
   const { data: user } = useQuery(['user'], () => getMe())
+
   const {
     data: tickets,
     isFetching: isFetchingTickets,
     refetch: refetchTickets,
-  } = useQuery(['tickets', user], () => getTickets(undefined, user?.id))
+  } = useQuery(['tickets', user], () => getTickets(undefined, user?.id), {
+    onSuccess: (res) => {
+      const hoge = res.map((ticket) => {
+        return {
+          id: String(ticket.id),
+          title: ticket.title,
+          start: ticket.startAt,
+          end: ticket.endAt,
+          userId: ticket.User?.id,
+        }
+      })
+      setEvents(hoge)
+    },
+  })
 
   const handleClickTicketCard = useCallback((ticket: Ticket) => {
     setOpenEditDrawer(true)
     setClickedTicket(ticket)
   }, [])
+
+  const handleClickEventCard = (id: string) => {
+    console.log(id)
+    const obj = tickets?.find((ticket) => ticket.id === Number(id))
+    setOpenEditDrawer(true)
+    setClickedTicket(obj)
+    void refetchTickets()
+  }
 
   return (
     <>
@@ -78,22 +110,19 @@ export const Home: React.FC = () => {
           <div className={styles.calendar}>
             <FullCalendar
               height="100%"
-              plugins={[timeGridPlugin, interactionPlugin]}
+              plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
               initialView="timeGridDay"
               locales={[jaLocale]}
               locale="ja"
               headerToolbar={{
-                left: '',
+                left: 'dayGridMonth,timeGridWeek,timeGridDay',
                 center: '',
                 right: 'prev,next',
               }}
-              events={[
-                {
-                  title: '要件定義書を作成',
-                  start: '2023-08-01T10:00:00',
-                  end: '2023-08-01T14:00:00',
-                },
-              ]}
+              events={events}
+              eventClick={(arg) => {
+                handleClickEventCard(arg.event.id)
+              }}
             />
           </div>
         </div>
